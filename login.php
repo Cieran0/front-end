fc7f166cfc65f4791e0def8bc46cd0cd2009f57d
Michael <190006115@dundee.ac.uk>
2024-11-06 14:13:19 +0000

    got yo

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="login.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in page</title>
+</head>
+<body>
+    
+    
+<form action="/action_page.php" method="post">
+
+  <div class="container">
+    <label for="uname"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+        
+    <button type="submit">Login</button>
+  </div>
+
+
+</form>
+
+</body>
+</html>
