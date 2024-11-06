e5f8257d7494a00003b6ebfa1e613654e988d14f
BenH9999 <ben.houghton7070@outlook.com>
2024-11-06 15:26:09 +0000

    css stuff

--- index.php
+++ index.php
@@ -4,11 +4,28 @@
             Tech Supply
         </title>
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="color.css">
     </head>
 
     <body>
-        <div class="header">
+        <div class="header container">
             <img class="logo" src="logo.png">
+            <div class="searchBar">
+                <input id="searchBar" type="text">
+                <button id="searchButton">Search</button>
+            </div>
+
+            <button id="login" onclick="document.location.href = 'login.php'">Login</button>
+        </div>
+
+        <div class="footer container">
+            <p id="support">
+                Customer Support:
+            </p>
+            <p id="info">
+                email: email@email.com<br> 
+                telephone: 4749479474
+            </p>
         </div>
     </body>
 </html>
