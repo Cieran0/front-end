d5381c225158911c66419a100487a95c5159a539
M-Harley01 <190006115@dundee.ac.uk>
2024-11-13 14:30:38 +0000

    bookmarking implemented

--- website/bulma/view_bookmarks.php
+++ website/bulma/view_bookmarks.php
@@ -15,10 +15,46 @@
   <?php
   
   session_start();
-  @include_once 'header.php'
-  
+  @include_once 'header.php';
+  @include_once 'query.php';
 
-  $sqlView = query();
+  $sqlView = query("SELECT * FROM Bookmarks WHERE CustomerID = ". $_SESSION['CustomerID'] .";");
+
+  if(mysqli_num_rows($sqlView)> 0) {
+    echo "    <section class=\"section\">
+<p class=\"title\">Your Recommended:</p>
+<div class=\"container\">
+<div class=\"columns is-multiline\">";
+
+    while ($row =mysqli_fetch_array($sqlView)) {
+      $pid = $row["ProductID"];
+      $name = $row["ProductName"];
+      $price = $row["BasePrice"];
+      $img = "product.png";
+
+      echo"<div class=\"column is-one-fifth is-third-mobile is-one-third-tablet is-one-fifth-desktop\">
+<div class=\"card\">
+<div class=\"card-image\">
+  <figure class=\"image is-1by1\">
+    <a href=\"product.php?ProductID=". $pid ."\">
+    <img src=\"$img\" alt=\"Placeholder image\">
+    </a>
+  </figure>
+</div>
+<div class=\"card-content\">
+  <div class=\"content\">
+    <p><strong>$name</strong></p>
+    <p>$price</p>
+  </div>
+</div>
+</div>
+</div>";
+  }
+
+  echo "        </div>
+  </div>
+</section>";
+}
 
   ?>
     
